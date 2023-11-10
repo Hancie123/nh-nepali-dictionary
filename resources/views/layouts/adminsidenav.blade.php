cedab2db593983219dec373b8e60a7fdf54f0eac
Hancie123 <hanciewanemphago@gmail.com>
2023-11-11 00:21:41 +0545

    dashboard updated

--- resources/views/layouts/adminsidenav.blade.php
+++ resources/views/layouts/adminsidenav.blade.php
@@ -25,7 +25,7 @@
                     <input type="text" name="footer-email" class="form-control">
                     <span class="form-bar"></span>
                     <label class="float-label"><i class="fa fa-search m-r-10"></i>Search
-                        Friend</label>
+                        </label>
                 </div>
             </form>
         </div>
@@ -40,49 +40,59 @@
 
 
             </li>
-            <li class="{{ request()->is('admin/setting') ? 'active' : null }}">
-                <a href="{{ url('admin/setting') }}" class="waves-effect waves-dark">
-                    <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
-                    <span class="pcoded-mtext">Setting</span>
-                    <span class="pcoded-mcaret"></span>
-                </a>
-            </li>
-        </ul>
-        <div class="pcoded-navigation-label">UI Element</div>
-        <ul class="pcoded-item pcoded-left-item">
-            <li class="pcoded-hasmenu">
-                <a href="javascript:void(0)" class="waves-effect waves-dark">
-                    <span class="pcoded-micon"><i class="ti-layout-grid2-alt"></i><b>BC</b></span>
-                    <span class="pcoded-mtext">Basic</span>
-                    <span class="pcoded-mcaret"></span>
-                </a>
-                <ul class="pcoded-submenu">
-                    <li class=" ">
-                        <a href="breadcrumb.html" class="waves-effect waves-dark">
-                            <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
-                            <span class="pcoded-mtext">Breadcrumbs</span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
-                    <li class=" ">
-                        <a href="button.html" class="waves-effect waves-dark">
-                            <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
-                            <span class="pcoded-mtext">Button</span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
-                    <li class="">
-                        <a href="accordion.html" class="waves-effect waves-dark">
-                            <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
-                            <span class="pcoded-mtext">Accordion</span>
-                            <span class="pcoded-mcaret"></span>
-                        </a>
-                    </li>
+            <ul class="pcoded-item pcoded-left-item">
+                <li class="pcoded-hasmenu">
+                    <a href="javascript:void(0)" class="waves-effect waves-dark">
+                        <span class="pcoded-micon"><i class="ti-layout-grid2-alt"></i><b>BC</b></span>
+                        <span class="pcoded-mtext">Dictionary</span>
+                        <span class="pcoded-mcaret"></span>
+                    </a>
+                    <ul class="pcoded-submenu">
+                        <li class=" ">
+                            <a href="breadcrumb.html" class="waves-effect waves-dark">
+                                <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
+                                <span class="pcoded-mtext">Upload Meaning</span>
+                                <span class="pcoded-mcaret"></span>
+                            </a>
+                        </li>
+                        <li class=" ">
+                            <a href="button.html" class="waves-effect waves-dark">
+                                <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
+                                <span class="pcoded-mtext">My Meaning</span>
+                                <span class="pcoded-mcaret"></span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a href="accordion.html" class="waves-effect waves-dark">
+                                <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
+                                <span class="pcoded-mtext">Meaning Mangement</span>
+                                <span class="pcoded-mcaret"></span>
+                            </a>
+                        </li>
 
 
-                </ul>
-            </li>
+                    </ul>
+                </li>
+                <li class="{{ request()->is('admin/setting') ? 'active' : null }}">
+                    <a href="{{ url('admin/setting') }}" class="waves-effect waves-dark">
+                        <span class="pcoded-micon"><i class="ti-settings"></i><b>D</b></span>
+                        <span class="pcoded-mtext">Setting</span>
+                        <span class="pcoded-mcaret"></span>
+                    </a>
+                </li>
+                <li class="{{ request()->is('logout') ? 'active' : null }}">
+                    <a href="{{ url('logout') }}" class="waves-effect waves-dark">
+                        <span class="pcoded-micon"><i class="ti-close"></i><b>D</b></span>
+                        <span class="pcoded-mtext">Logout</span>
+                        <span class="pcoded-mcaret"></span>
+                    </a>
+                </li>
+            </ul>
+
+
         </ul>
+
+
 
 
     </div>
