d1002c89f2db00790e47bb5edd57b09a18a1e944
Hancie123 <hanciewanemphago@gmail.com>
2023-11-17 10:47:55 +0545

    about us updated

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -18,7 +18,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-7 text-center mx-auto">
-                        <h1 class="text-white pt-3 mt-n5">Terms and Conditions</h1>
+                        <h1 class="text-white pt-3 mt-n5">About Us</h1>
 
                     </div>
                 </div>
@@ -51,19 +51,26 @@
                 <div class="col-lg-9 z-index-2 border-radius-xl mt-n10 mx-auto py-3 blur shadow-blur">
 
 
-                        <div class="container py-5">
-                          <div class="row justify-content-center">
+                    <div class="container py-5">
+                        <div class="row justify-content-center">
                             <div class="col-xl-6 col-lg-9 col-md-10 col-12 text-center">
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="d-inline-block w-8 h-8 text-secondary mb-4" viewBox="0 0 975.036 975.036">
-                                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-                              </svg>
-                              <p class="lead">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking vinegar.</p>
-                              <hr class="bg-indigo-500 h-1 w-25 rounded mt-4 mb-4">
-                              <h2 class="text-dark font-weight-medium h4">HOLDEN CAULFIELD</h2>
-                              <p class="text-secondary">Senior Product Designer</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    class="d-inline-block w-8 h-8 text-secondary mb-4" viewBox="0 0 975.036 975.036">
+                                    <path
+                                        d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z">
+                                    </path>
+                                </svg>
+                                <p class="lead text-dark">The NH Nepali Dictionary is a web and mobile dictionary application
+                                    designed by Hancie Phago. This dictionary application is focused on the Nepali
+                                    language and provides users with a comprehensive collection of over 20,000 words and
+                                    their meanings. One of its key features is the translation from English to Nepali,
+                                    allowing users to easily understand and communicate in both languages.</p>
+                                <hr class="bg-indigo-500 h-1 w-25 rounded mt-4 mb-4">
+                                <h2 class="text-dark font-weight-medium h4">Hancie Phago</h2>
+                                <p class="text-dark">Software Engineer</p>
                             </div>
-                          </div>
                         </div>
+                    </div>
 
 
 
@@ -92,7 +99,7 @@
 
 
 
-@include('layouts/footer')
+    @include('layouts/footer')
 </body>
 
 </html>
